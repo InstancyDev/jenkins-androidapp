9f977d05fc4e7d6f5c97f65c772aab446cc5fca3
Upendranath <upendranath@Instancy.com>
2018-11-13 17:37:53 +0530

    AskExpert new design and discussion forum new design

---
+++ app/src/main/java/com/instancy/instancylearning/myskills/MySkillModel.java
@@ -0,0 +1,41 @@
+package com.instancy.instancylearning.myskills;
+
+import org.json.JSONArray;
+
+import java.io.Serializable;
+
+/**
+ * Created by Upendranath on 6/8/2017 Working on InstancyLearning.
+ */
+
+public class MySkillModel implements Serializable {
+
+
+    public String skillName = "";
+    public String skillID = "";
+    public String prefCategoryID = "";
+    public String jobRoleID = "";
+    public String skillDescription = "";
+    public String requiredProfValues = "";
+    public String userPictureURL = "";
+    public String managerPictureURL = "";
+    public String contentAuthorPictureURL = "";
+    public double requiredProficiency = 0.0;
+    public int requiredScore = 0;
+    public String valueName = "";
+    public double gapScore = 0.0;
+    public double weightedAverage = 0.0;
+    public int userScore = 0;
+    public int managerScore = 0;
+    public double contentAuthorScore = 0.0;
+    public JSONArray requiredProfArys = null;
+    public String viewContent = "true";
+    
+    public String weightedAverageStr = "";
+    public String managerScoreStr = "";
+    public String userScoreStr = "";
+    public String contentAuthorScoreStr = "";
+
+    public String managerEvaluation = "";
+
+}
